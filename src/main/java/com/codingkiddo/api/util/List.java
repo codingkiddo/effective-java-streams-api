ef2fe9ef0b64b638666148db6b83d73f27e051c9
Vinod Kumar <vinodkumar@192.168.1.2>
2025-11-12 19:28:07 +0530

    modified:   src/main/java/com/codingkiddo/api/util/AbstractCollection.java 	new file:   src/main/java/com/codingkiddo/api/util/AbstractList.java 	modified:   src/main/java/com/codingkiddo/api/util/Iterable.java 	new file:   src/main/java/com/codingkiddo/api/util/List.java 	modified:   src/main/java/com/codingkiddo/api/util/streams/function/Function.java 	new file:   src/main/java/com/codingkiddo/api/util/streams/function/UnaryOperator.java

---
+++ src/main/java/com/codingkiddo/api/util/List.java
@@ -0,0 +1,50 @@
+package com.codingkiddo.api.util;
+
+import java.util.Objects;
+
+public interface List<E> extends Collection<E> {
+
+	 /**
+     * Returns the number of elements in this list.  If this list contains
+     * more than <tt>Integer.MAX_VALUE</tt> elements, returns
+     * <tt>Integer.MAX_VALUE</tt>.
+     *
+     * @return the number of elements in this list
+     */
+    int size();
+
+    /**
+     * Returns <tt>true</tt> if this list contains no elements.
+     *
+     * @return <tt>true</tt> if this list contains no elements
+     */
+    boolean isEmpty();
+    
+    boolean contains(Object o);
+
+    /**
+     * Returns an iterator over the elements in this list in proper sequence.
+     *
+     * @return an iterator over the elements in this list in proper sequence
+     */
+    Iterator<E> iterator();
+    
+    Object[] toArray();
+    
+    <T> T[] toArray(T[] a);
+    boolean add(E e);
+    boolean remove(Object o);
+    boolean containsAll(Collection<?> c);
+    boolean addAll(Collection<? extends E> c);
+    boolean addAll(int index, Collection<? extends E> c);
+    boolean removeAll(Collection<?> c);
+    boolean retainAll(Collection<?> c);
+ 
+    default void replaceAll(UnaryOperator<E> operator) {
+        Objects.requireNonNull(operator);
+        final ListIterator<E> li = this.listIterator();
+        while (li.hasNext()) {
+            li.set(operator.apply(li.next()));
+        }
+    }
+}
--- src/main/java/com/codingkiddo/api/util/streams/function/Function.java
+++ src/main/java/com/codingkiddo/api/util/streams/function/Function.java
@@ -8,22 +8,6 @@ public interface Function<T, R> {
 
 	R apply(T t);
 	
-	class Util {
 	
-		private Util() { }
-		
-		public static <T, R, V> Function<T, V> andThen(
-                @NotNull final Function<? super T, ? extends R> f1,
-                @NotNull final Function<? super R, ? extends V> f2) {
-            Objects.requireNonNull(f1, "f1");
-            Objects.requireNonNull(f2, "f2");
-            return new Function<T, V>() {
-                @Override
-                public V apply(T t) {
-                    return f2.apply(f1.apply(t));
-                }
-            };
-        }
-	}
 	
 }
