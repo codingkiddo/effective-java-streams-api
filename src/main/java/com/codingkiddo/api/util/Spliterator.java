9fb0de6d3020e187cb234c019af0d7357373d770
Vinod Kumar <vinodkumar@192.168.1.14>
2025-11-13 14:01:56 +0530

    new file:   src/main/java/com/codingkiddo/api/examples/ArrayListMain.java 	modified:   src/main/java/com/codingkiddo/api/util/Spliterator.java

--- src/main/java/com/codingkiddo/api/util/Spliterator.java
+++ src/main/java/com/codingkiddo/api/util/Spliterator.java
@@ -1,5 +1,39 @@
 package com.codingkiddo.api.util;
 
+import java.util.Comparator;
+
+import com.codingkiddo.api.util.streams.function.Consumer;
+
 public interface Spliterator<T> {
 
+	public static final int ORDERED    = 0x00000010;
+    public static final int DISTINCT   = 0x00000001;
+    public static final int SORTED     = 0x00000004;
+    public static final int SIZED      = 0x00000040;
+    public static final int NONNULL    = 0x00000100;
+    public static final int IMMUTABLE  = 0x00000400;
+    public static final int CONCURRENT = 0x00001000;
+    public static final int SUBSIZED = 0x00004000;
+    
+	boolean tryAdvance(Consumer<? super T> action);
+	
+	default void forEachRemaining(Consumer<? super T> action) {
+		do {} while(tryAdvance(action));
+	}
+	
+	Spliterator<T> trySplit();
+	
+	long estimateSize();
+	
+	int characteristics();
+	
+	default boolean hasCharacteristics(int characteristics) {
+		return ( characteristics() & characteristics) == characteristics;
+	}
+	
+	default Comparator<? super T> getComparator() {
+		throw new IllegalStateException();
+	}
+	
+	
 }
